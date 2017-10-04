986c3a7d409c6b34bf56cc84d563f84ffafaf6c4
Tri Hoang <hmtri1011@gmail.com>
2017-10-04 10:11:27 +0700

    update deploy command script

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
 #!/usr/bin/env node
 const cmd = require('node-cmd')
+const co = require('co')
+const prompt = require('co-prompt')
 
-// change foxplus-production to foxplus-staging if want to deploy staging
-cmd.get(
-  `eb use foxplus-production
-  git pull
-  yarn build
-  git add dist -f
-  git commit -m "Build production"
-  git push
-  eb deploy`,
-  (err, data, stderr) => {
-    if (!err) {
-      console.log('Deployed!')
-    } else {
-      console.log('error', err)
-    }
+// git push
+//                   eb deploy
+
+const deployment = ebEnv => {
+  return co(function*() {
+    var mess = yield prompt('Enter your commit message: ')
+    cmd.get(
+      `eb use ${ebEnv}
+      git pull
+      yarn build
+      `,
+      function(err, data, stderr) {
+        if (!err) {
+          cmd.run(`NODE_ENV=staging yarn start`)
+          console.log('Please check on localhost:3000 before deploy')
+          const confirm = new Promise(resolve =>
+            resolve(prompt('Do you want to deploy now (y/n): '))
+          )
+
+          confirm.then(confirmData => {
+            co(function*() {
+              const ok = yield confirmData
+              if (ok.toLowerCase() === 'y' || ok.toLowerCase() === 'yes') {
+                cmd.get(
+                  `git add dist -f
+                  git commit -m '${mess}'
+                  eb status`,
+                  (err, data, stderr) => {
+                    if (!err) {
+                      console.log(data, '\n')
+                      console.log('Deployed')
+                      process.exit()
+                    } else {
+                      console.log(err)
+                      process.exit()
+                    }
+                  }
+                )
+              } else {
+                console.log('You dont want to deploy, see yaa!')
+                process.exit()
+              }
+            })
+          })
+        } else {
+          console.log('error', err)
+          process.exit()
+        }
+      }
+    )
+  })
+}
+
+co(function*() {
+  var branch = yield prompt(
+    'Please choose your enviroment for deploying\n1: Production \t\t 2: Staging\nYour anwser: '
+  )
+  if (branch === 1 || branch === '1') {
+    cmd.get(
+      `git checkout master
+      git pull`,
+      (err, data, stderr) => {
+        if (!err) {
+          console.log(data)
+          return deployment('foxplus-production')
+        }
+        console.log(err)
+        process.exit()
+      }
+    )
+  } else {
+    cmd.get(
+      `git checkout develop
+      git pull`,
+      (err, data, stderr) => {
+        if (!err) {
+          console.log(data)
+          return deployment('foxplus-staging')
+        }
+        console.log(err)
+        process.exit()
+      }
+    )
   }
-)
+})
